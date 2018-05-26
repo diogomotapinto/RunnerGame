4c9743b8a98d739d7cd678f605ae0801fd659764
Diogo Mota Pinto <diogomotapinto@gmail.com>
2018-05-26 17:09:00 +0100

    Starting tests

---
+++ core/test/com/mygdx/game/controller/GameControllerTest.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.controller;
+
+import org.junit.Test;
+import com.mygdx.game.controller.GameTest;
+
+public class GameControllerTest extends GameTest {
+
+    GameTest tester = new GameTest();
+
+    @Test
+    public void getScore() {
+       
+    }
+
+    @Test
+    public void jump() {
+
+    }
+
+    @Test
+    public void run() {
+
+    }
+
+    @Test
+    public void shoot() {
+
+    }
+
+    @Test
+    public void removeBody() {
+
+    }
+}
